49c16b4ceb4cdb3e4b7e33e717d1240ef20d3a2b
Evan Kristian <evan.kristian@gcloud.co.id>
2011-11-23 13:25:34 +0700

    f

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Semantic::Application.routes.draw do
-  get "dashboards/index"
+  resources :dashboards
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +50,7 @@ Semantic::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'dashboards#index'
 
   # See how all your routes lay out with "rake routes"
 
