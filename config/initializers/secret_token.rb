103ea1285cde8ff453f0138b62482b7541a0786e
Evan Kristian <evan.kristian@gcloud.co.id>
2011-11-23 11:22:12 +0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Semantic::Application.config.secret_token = '9ce34ed82f90545c7c33cbb196da8a111a79401d15c7d17bcfbbac56fbdc0c66dfe9c2856c77ce1cc65865b1ae6e8a812d1c1c715d3920f838fa99533ad52a39'
