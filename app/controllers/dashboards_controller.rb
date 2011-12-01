42aef8b84c371e1a3295c76fb10d18384e1d09e3
Evan David Kristian <evan@localhost.localdomain>
2011-12-02 04:25:03 +0700

    d

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,8 +3,8 @@ class DashboardsController < ApplicationController
   	if current_facebook_user
   		require 'open-uri'
 			require 'json'
-  		#@friends= JSON.parse(open("https://graph.facebook.com/me/home?access_token="+current_facebook_user.client.access_token).read)
-  		#@friends = @friends["data"]
+  		@friends= JSON.parse(open("https://graph.facebook.com/me/home?access_token="+current_facebook_user.client.access_token).read)
+  		@friends = @friends["data"]
   	end 
   end
 
